155ba0a8057d26e0a8c83092cdc43eab9a319c66
SJBroomfield <stephen@broomus.com>
2020-02-09 19:22:08 -0500

    Made it so shooter doesn't start rolling automatically, changed the shooter speed input to -.50 for testing purposes

--- src/main/java/frc/robot/commands/shootCommand.java
+++ src/main/java/frc/robot/commands/shootCommand.java
@@ -42,11 +42,13 @@ public class shootCommand extends Command {
   // Called once after isFinished returns true
   @Override
   protected void end() {
+    Robot.robotmap.shootCtrl.set(0);
   }
 
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
+    Robot.robotmap.shootCtrl.set(0);
   }
 }
