d977343dfd40bb9d4a8ff162e79b299b5976ccde
SJBroomfield <stephen@broomus.com>
2020-03-05 20:43:50 -0500

    Added test auto commands, not utilized yet

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -165,13 +165,13 @@ public class Robot extends TimedRobot { // does not actually mean TimedRobot!!!
     char autoPositionChar = autoChar(getString("M").charAt(0));//This code is currently not working because no NetworkTables entry for the autoChar string have been made
     switch(autoPositionChar)
     {
-      case 1: ;//calls the auto command for the left position
+      case 'L': ;//calls the auto command for the left position
       break;
 
-      case 2: ;//calls the auto command for the middle position
+      case 'M': ;//calls the auto command for the middle position
       break;
 
-      case 3: ;//calls the auto command for the right position
+      case 'R': ;//calls the auto command for the right position
       break;
     }
 
---
+++ src/main/java/frc/robot/commandGroups/AutoMoveForward.java
@@ -0,0 +1,35 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commandGroups;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.robot.Robot;
+import frc.robot.autoCommands.autoDriveForward;
+
+public class AutoMoveForward extends CommandGroup {
+  /**
+   * Add your docs here.
+   */
+  public AutoMoveForward() {
+
+    requires(Robot.drive_sub);
+    // Add Commands here:
+    // e.g. addSequential(new Command1());
+    // addSequential(new Command2());
+    // these will run in order.
+    addSequential(new autoDriveForward(), 1);
+
+    /* To run multiple commands at the same time,
+    // use addParallel()
+    // e.g. addParallel(new Command1());
+    // addSequential(new Command2());
+    // Command1 and Command2 will run in parallel.
+    */
+
+  }
+}
